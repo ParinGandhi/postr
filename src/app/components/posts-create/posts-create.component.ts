e82dbece988aa15e7badc3adda935686662058a4
Parin Gandhi <parin.a.gandhi@gmail.com>
2019-03-30 23:06:32 -0400

    Added event listener

--- src/app/components/posts-create/posts-create.component.ts
+++ src/app/components/posts-create/posts-create.component.ts
@@ -7,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PostsCreateComponent implements OnInit {
 
+  onAddPost() {
+    alert("Post added!");
+  }
+
   constructor() { }
 
   ngOnInit() {
